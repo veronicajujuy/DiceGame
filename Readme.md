fcdf613115c96b9a156a3428fcfe635a158989b3
veronicajujuy <veronicajujuy@gmail.com>
2022-06-20 17:13:28 -0300

    dice game

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# Dice Game Challenge!
+
+## This is a solution for the dice Game Challenge of the Web Developer BootCamp of App Brewery
+
+This game present two dices. When you refresh the page, shows who wins!
+
+![](images/capture_site.png)
+
+Enyoy!
---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNumber1 = Math.ceil(Math.random()*6)
+var randomNumber2 = Math.ceil(Math.random()*6)
+
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png")
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png")
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🏅Player 1 Wins!"
+} else if ((randomNumber1 < randomNumber2)) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🏅"
+} else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
